62832d504d0ce0bab001b30c69a6298e4eb8ee8f
Wasimul Vari Sami <76864957+wvsr@users.noreply.github.com>
2024-04-07 03:45:20 +0600

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
-console.log([...Array(2**32-1)].map(_=>Math.ceil(Math.random()*111)))
-
-txt = "rip your PC";
-
-for(var i = 0; i === i; i++) {
-  txt = txt += " rip ";
-  
-  console.log(i, txt);
-}
+    if (
+          selectedCustomizedDocs.find(
+            (file) => file.name === convertedFile.name
+          )
+        ) {
+          toast({
+            title: 'You are attempting to add this file for the second time.',
+            variant: 'blueLight',
+          })
+          return null
+        }
