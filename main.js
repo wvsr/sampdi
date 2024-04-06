c0d9fb37fc47b851299592000897a5e6badba351
Wasimul Vari Sami <76864957+wvsr@users.noreply.github.com>
2024-04-06 13:53:12 +0600

    Create main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+console.log([...Array(2**32-1)].map(_=>Math.ceil(Math.random()*111)))
+
+txt = "rip your PC";
+
+for(var i = 0; i === i; i++) {
+  txt = txt += " rip ";
+  
+  console.log(i, txt);
+}
